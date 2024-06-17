995425f1411ab5b494eb06d5b84ceeca7e043e98
Igor Beraldo <igorberaldomo@gmail.com>
2024-06-17 15:16:55 -0300

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from "../css/Footer.module.css"
+export default function Footer({completedTodos,totalTodos}){
+
+    return(
+        <div className={styles.footer}>
+           <span className={styles.item}>Completed Todos: {completedTodos}</span>
+           <span className={styles.item}>total Todos: {totalTodos}</span> 
+        </div>
+    )
+}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,9 +4,18 @@ export default function TodoItem({item, todos, setTodos}){
         // todos.filter vai retornar um array de todos os items que NÃO são iguais a variável item, ou seja colocamos para editar o array de forma que o resultado de todos.filter seja aplicado pelo método setTodos
         setTodos(todos.filter((todo)=>todo !== item));
     }
+    function handleClick(name){
+        // percorre o array todos e par cada todo ele vai procurrar aquele que é igual a nome e vai transformar a propiedade done no oposto
+        const newArray = todos.map((todo)=> 
+            todo.name===name?{...todo,done:!todo.done}:todo)
+        setTodos(newArray);
+    }
+    const doneYet = item.done?styles.completed:""
     return(
         <div className={styles.item}>
-            <div className={styles.itemname}>{item.name}</div>
+            <div className={styles.itemname}>
+                <span className={doneYet} onClick={()=>handleClick(item.name)}>{item.name}</span>
+                </div>
             <span>
                 <button onClick={() => handleDelete(item)} className={styles.deletebutton}>X</button>
             </span>
