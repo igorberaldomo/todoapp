fbff02b6e48b9c74429e267dd07470f6f96a5ed0
Igor Beraldo <igorberaldomo@gmail.com>
2024-06-17 11:43:09 -0300

    button fix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import styles from "../css/Form.module.css"
 export default function Form({todos,setTodos}){
-    const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState({name:"",done:false});
     function handleSubmit(e) {
         e.preventDefault();
         setTodos([...todos, todo]);
-        setTodo("")
+        setTodo({name:e.target.value, done:false})
     }
     return(
         <form className = {styles.todoform} onSubmit={handleSubmit}>
                <div className={styles.container}> 
-                    <input className = {styles.moderninput} onChange={(e) => setTodo(e.target.value)} type="text" value={todo} placeholder="Enter todo item..." />
+                    <input className = {styles.moderninput} onChange={(e) => setTodo({name:e.target.value, done:false})} type="text" value={todo.name} placeholder="Enter todo item..." />
                     <button className={styles.modernbutton} type="submit">Add</button>
                 </div>
             </form>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,11 +2,11 @@ import styles from "../css/TodoItem.module.css"
 export default function TodoItem({item, todos, setTodos}){
     function handleDelete(item){
         // todos.filter vai retornar um array de todos os items que NÃO são iguais a variável item, ou seja colocamos para editar o array de forma que o resultado de todos.filter seja aplicado pelo método setTodos
-        setTodos(todos.filter((todo)=>todo !== item))
+        setTodos(todos.filter((todo)=>todo !== item));
     }
     return(
         <div className={styles.item}>
-            <div className={styles.itemname}>{item}</div>
+            <div className={styles.itemname}>{item.name}</div>
             <span>
                 <button onClick={() => handleDelete(item)} className={styles.deletebutton}>X</button>
             </span>
